3a3896ca9234c19772641c8d44f11ca42865b457
Liubov Jones <elljayua@gmail.com>
2026-07-16 15:31:25 +0100

    Added file storage functionality

--- AC2_3.py
+++ AC2_3.py
@@ -1,10 +1,14 @@
 def get_name():
     name = input("Enter your name: ")
     return name
+def save_name(name):
+    with open("users.txt", "a") as file:
+        file.write(name + "\n")
 def display_menu():
     print("\nMain Menu")
     print("1. Display Welcome Message")
-    print("2. Exit")
+    print("2. Save Name")
+    print("3. Exit")
 user_name = get_name()
 while True:
     display_menu()
@@ -12,7 +16,10 @@ while True:
     if choice == "1":
         print(f"Welcome, {user_name}!")
     elif choice == "2":
-        print("Goodbye!")
+        save_name(user_name)
+        print("Name now saved!")
+    elif choice == "3":
+        print("Bye Bye :) !")
         break
     else:
-        print("Please choose 1 or 2.")
+        print("Please choose 1, 2 or 3.")
