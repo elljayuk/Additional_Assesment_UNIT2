094c81b6f5088caef78e58aba5eef5375e91fbd4
Liubov Jones <elljayua@gmail.com>
2026-07-16 15:27:14 +0100

    Improved app navigation

--- AC2_3.py
+++ AC2_3.py
@@ -1,5 +1,18 @@
 def get_name():
     name = input("Enter your name: ")
-    print(f"Welcome, {name}!")
-
-get_name()
+    return name
+def display_menu():
+    print("\nMain Menu")
+    print("1. Display Welcome Message")
+    print("2. Exit")
+user_name = get_name()
+while True:
+    display_menu()
+    choice = input("Choose an option: ")
+    if choice == "1":
+        print(f"Welcome, {user_name}!")
+    elif choice == "2":
+        print("Goodbye!")
+        break
+    else:
+        print("Please choose 1 or 2.")
